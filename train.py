9ec6be61f3a4ecb6bf68596837aa7582a88fec51
Konstantinos Peratinos <kotsosgr@gmail.com>
2019-03-25 23:25:10 +0100

    train file done

--- train.py
+++ train.py
@@ -10,6 +10,17 @@ from sklearn.linear_model import LogisticRegression
 
 # add whatever additional imports you may need here.
 
+def removeClass(itemList):
+    result = []
+    N = len(itemList[0])
+    print(N)
+    for i in range(len(itemList)):
+        temp = []
+        for j in range(0,N-1):
+           temp+=itemList[i][j]
+        result.append(temp)
+    return result
+
 parser = argparse.ArgumentParser(description="Train a maximum entropy model.")
 parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
 parser.add_argument("datafile", type=str,
@@ -20,15 +31,25 @@ parser.add_argument("modelfile", type=str,
 args = parser.parse_args()
 
 print("Loading data from file {}.".format(args.datafile))
+### Reading the File
 itemList = []
-with open(args.outputfile+".train", "rb") as rb:
+with open(args.datafile+".train", "rb") as rb:
     itemList = pickle.load(rb)
-print(itemList)
-rb.close()
 
+itemList_Y = [itemList[x][args.ngram-1] for x in range(0, len(itemList)) ]
+itemList_X = removeClass(itemList)
+print(itemList_X[0])
+#print(itemList_X[0])
 print("Training {}-gram model.".format(args.ngram))
+### Logisting Regression
+#print(len(itemList_Y))
+#print(len(itemList))
+clf = LogisticRegression(multi_class='multinomial', solver='lbfgs').fit(itemList_X, itemList_Y)
 print("Writing table to {}.".format(args.modelfile))
-
+with open(args.modelfile, "wb+") as testFile:
+    pickle.dump(clf, testFile)
+testFile.close()
+rb.close()
 # YOU WILL HAVE TO FIGURE OUT SOME WAY TO INTERPRET THE FEATURES YOU CREATED.
 # IT COULD INCLUDE CREATING AN EXTRA COMMAND-LINE ARGUMENT OR CLEVER COLUMN
 # NAMES OR OTHER TRICKS. UP TO YOU.
